1e644e8f0253d3ce69f79ff2b11f911d83360325
vonkka <vonkka@mail.ru>
2022-09-15 22:38:11 +0300

    quickSort algorithm first version

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -4,12 +4,33 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * Created by artemaliev on 07/09/15.
- */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
+  private static void swap(int array[], int j, int i) {
+    if (i != j) {
+      int temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+  private static int partition(int array[], int beg, int end) {
+    int partElem = array[end];
+    int j = beg;
+    for (int i = beg; i <= end; ++i) {
+      if (array[i] < partElem) swap(array, i, j++);
+    }
+    swap(array, j, end);
+    return j;
+  }
+
+  private static void quickSort(int array[], int beg, int end) {
+    if (beg < end) {
+      int partElem = partition(array, beg, end);
+      quickSort(array, beg, partElem - 1);
+      quickSort(array, partElem + 1, end);
+    }
+  }
+  public static void sort(int array[]) {
+    quickSort(array, 0, array.length - 1);
   }
 
   public static void sort (List<Integer> list) {
