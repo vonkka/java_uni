3e176759c9f2b558054b5545c5788213ef1a5343
vonkka <vonkka@mail.ru>
2022-09-17 00:49:01 +0300

    quickSort algorithm first version

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -1,35 +1,49 @@
 package edu.spbu.sort;
+import java.util.Random;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class IntSort {
-  private static void swap(int array[], int j, int i) {
+  public static int avg(int a, int b, int c) {
+    if (a > b) return b > c ? b : Math.min(a, c);
+    else if (a > c) return a;
+    return c;
+  }
+  public static void swap(int[] array, int j, int i) {
     if (i != j) {
       int temp = array[i];
       array[i] = array[j];
       array[j] = temp;
     }
   }
-  private static int partition(int array[], int beg, int end) {
-    int partElem = array[end];
-    int j = beg;
-    for (int i = beg; i <= end; ++i) {
-      if (array[i] < partElem) swap(array, i, j++);
+
+  public static int partition(int[] array, int beg, int end) {
+    int pivot = avg(array[beg], array[(end + beg) / 2], array[end]);
+    int i = beg;
+    for (int j = beg; j <= end; ++j) {
+      if (array[j] < pivot) {
+        swap(array, i++, j);
+      }
     }
-    swap(array, j, end);
-    return j;
+    for (int k = end; k > i; --k) {
+      if (array[k] == pivot) {
+        swap(array, k, i);
+        break;
+      }
+    }
+    return i;
   }
 
-  private static void quickSort(int array[], int beg, int end) {
+  public static void quickSort(int[] array, int beg, int end) {
     if (beg < end) {
-      int partElem = partition(array, beg, end);
-      quickSort(array, beg, partElem - 1);
-      quickSort(array, partElem + 1, end);
+      int ind = partition(array, beg, end);
+      quickSort(array, beg, ind - 1);
+      quickSort(array, ind + 1, end);
     }
   }
-  public static void sort(int array[]) {
+  public static void sort(int[] array) {
     quickSort(array, 0, array.length - 1);
   }
 
